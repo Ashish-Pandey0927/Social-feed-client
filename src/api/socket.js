51c68768c4e7003cc3d6bcd34200d394ea0896ac
Ashish Pandey <126936221+Ashish-Pandey0927@users.noreply.github.com>
2025-06-19 17:22:13 +0530

    First

--- src/api/socket.js
+++ src/api/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_SERVER_URL || "http://localhost:5000", {
+const socket = io(import.meta.env.VITE_SERVER_URL || "https://social-feed-server-xw5r.onrender.com", {
   transports: ["websocket"],
 });
 
